55250611830831f4e04faaf4e963c6d21f5afa2d
William Topping <William Topping>
2016-02-01 15:53:54 -0600

    Broken compile inside VS - wdi-simple is base project

--- examples/wdi-simple.c
+++ examples/wdi-simple.c
@@ -20,12 +20,18 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <windows.h>
+
 #ifdef _MSC_VER
 #include "getopt/getopt.h"
 #else
 #include <getopt.h>
 #endif
+
+#include "wdi-simple.h"
 #include "libwdi.h"
+#include "zadig.h"
+
 
 #if defined(_PREFAST_)
 /* Disable "Banned API Usage:" errors when using WDK's OACR/Prefast */
@@ -40,41 +46,73 @@
  * Change these values according to your device if
  * you don't want to provide parameters
  */
-#define DESC        "Microsoft XBox Controller Type S"
-#define VID         0x045E
-#define PID         0x0289
+#define BOOTLOADER_DESC        "STM32  BOOTLOADER"
+#define BOOTLOADER_VID         0x0483
+#define BOOTLOADER_PID         0xDF11
+#define PEACHY_DESC        "Peachy Printer"
+#define PEACHY_VID         0x16D0
+#define PEACHY_PID         0x0AF3
 #define INF_NAME    "usb_device.inf"
 #define DEFAULT_DIR "usb_driver"
-
 
 void usage(void)
 {
 	printf("\n");
-	printf("-n, --name <name>          set the device name\n");
-	printf("-f, --inf <name>           set the inf name\n");
-	printf("-m, --manufacturer <name>  set the manufacturer name\n");
-	printf("-v, --vid <id>             set the vendor ID (VID, use 0x prefix for hex)\n");
-	printf("-p, --pid <id>             set the product ID (PID, use 0x prefix for hex)\n");
-	printf("-i, --iid <id>             set the interface ID (MI)\n");
-	printf("-t, --type <driver_type>   set the driver to install\n");
-	printf("                           (0=WinUSB, 1=libusb-win32, 2=libusbK, 3=usbser, 4=custom)\n");
-	printf("-w, --wcid                 use a WCID driver instead of a device-specific\n");
-	printf("                           one (WinUSB, libusb-win32 or libusbK only)\n");
-	printf("    --filter               use the libusb-win32 filter driver (requires -t1)\n");
-	printf("-d, --dest <dir>           set the extraction directory\n");
-	printf("-x, --extract              extract files only (don't install)\n");
-	printf("-c, --cert <certname>      install certificate <certname> from the\n");
-	printf("                           embedded user files as a trusted publisher\n");
-	printf("    --stealth-cert         installs certificate above without prompting\n");
-	printf("-s, --silent               silent mode\n");
-	printf("-b, --progressbar=[HWND]   display a progress bar during install\n");
-	printf("                           an optional HWND can be specified\n");
-	printf("-o, --timeout              timeout (in millis) to wait for any pending installations\n");
-	printf("-l, --log                  set log level (0 = debug, 4 = none)\n");
-	printf("-h, --help                 display usage\n");
+	printf("-v, --verbose       print a bunch of debug stuff during operation\n");
+	printf("-b, --bootloader    switch bootloader driver to WINUSB\n");
+	printf("-l, --list          list all peachy's states\n");
+	printf("-h, --help          display usage\n");
+	printf("-p, --pause         Pause at end of script to see output\n");
 	printf("\n");
+	printf("Press Enter to continue\n");
+	getchar(); //hacky pause (press enter)
+}
+void set_bootloader_to_winusb(int verbose) {
+	int opt_silent = verbose;
+	struct wdi_device_info *device, list;
+	char* inf_name = NULL;
+	
+	list = listDevices(verbose);
+	if (&list != NULL) {
+		oprintf("set_bootloader_to_winusb - valid list\n");
+		for (device = &list; device != NULL; device = device->next) {
+			if ((device->vid == BOOTLOADER_VID) && (device->pid == BOOTLOADER_PID)) {
+				inf_name = to_valid_filename(device->desc, ".inf");
+				if (inf_name == NULL) {
+					oprintf("'%s' is %s for a device name",
+						device->desc, (strlen(device->desc)>WDI_MAX_STRLEN) ? "too long" : "invalid");
+				}
+				oprintf("Using inf name: %s", inf_name);
+				//Got device as pointer to the bootloader stuff, now lets install things?
+			}
+		}
+	}
 }
 
+struct wdi_device_info listDevices(int verbose){
+
+	int opt_silent = verbose;
+	struct wdi_device_info *device, *list;
+	static struct wdi_options_create_list ocl = { 0 };
+
+	ocl.list_all = TRUE;
+	ocl.list_hubs = TRUE;
+	ocl.trim_whitespaces = TRUE;
+
+	if (wdi_create_list(&list, &ocl) == WDI_SUCCESS) {
+		for (device = list; device != NULL; device = device->next) {
+			oprintf("Found: %s @ (%04X:%04X)\n", device->desc, device->vid, device->pid);
+			if ((device->vid == BOOTLOADER_VID) && (device->pid == BOOTLOADER_PID)) {
+				printf("BOOTLOADER,DRIVER:%s,VERSION:%I64d\n",device->driver,device->driver_version);
+			}
+			else if ((device->vid == PEACHY_VID) && (device->pid == PEACHY_PID)) {
+				printf("PEACHY,DRIVER:%s,VERSION:%I64d\n", device->driver, device->driver_version);
+			}
+		}
+	}
+	return *list;
+
+}
 // from http://support.microsoft.com/kb/124103/
 HWND GetConsoleHwnd(void)
 {
@@ -93,108 +131,45 @@ HWND GetConsoleHwnd(void)
 
 int __cdecl main(int argc, char** argv)
 {
-	static struct wdi_device_info *ldev, dev = {NULL, VID, PID, FALSE, 0, DESC, NULL, NULL, NULL};
-	static struct wdi_options_create_list ocl = { 0 };
-	static struct wdi_options_prepare_driver opd = { 0 };
-	static struct wdi_options_install_driver oid = { 0 };
-	static struct wdi_options_install_cert oic = { 0 };
-	static int opt_silent = 0, opt_extract = 0, log_level = WDI_LOG_LEVEL_WARNING;
-	static BOOL matching_device_found;
-	int c, r;
+	static int opt_silent = 1, log_level = WDI_LOG_LEVEL_WARNING;
+	struct wdi_device_info *list;
+	int c;
+	BOOL list_usbs=FALSE, bootloaderWinusbInstall=FALSE;
+	BOOL pause=FALSE;
 	char *inf_name = INF_NAME;
 	char *ext_dir = DEFAULT_DIR;
 	char *cert_name = NULL;
 
 	static struct option long_options[] = {
-		{"name", required_argument, 0, 'n'},
-		{"inf", required_argument, 0, 'f'},
-		{"manufacturer", required_argument, 0, 'm'},
-		{"vid", required_argument, 0, 'v'},
-		{"pid", required_argument, 0, 'p'},
-		{"iid", required_argument, 0, 'i'},
-		{"type", required_argument, 0, 't'},
-		{"filter", no_argument, 0, 2},
-		{"wcid", no_argument, 0, 'w'},
-		{"dest", required_argument, 0, 'd'},
-		{"cert", required_argument, 0, 'c'},
-		{"extract", no_argument, 0, 'x'},
-		{"silent", no_argument, 0, 's'},
-		{"stealth-cert", no_argument, 0, 1},
-		{"progressbar", optional_argument, 0, 'b'},
-		{"log", required_argument, 0, 'l'},
-		{"timeout", required_argument, 0, 'o'},
+		{"verbose", no_argument, 0, 'v'},
+		{"list", no_argument, 0, 'l'},
 		{"help", no_argument, 0, 'h'},
+		{"pause", no_argument, 0, 'p'},
+		{"bootloader", no_argument, 0, 'b'},
 		{0, 0, 0, 0}
 	};
 
-	ocl.list_all = TRUE;
-	ocl.list_hubs = TRUE;
-	ocl.trim_whitespaces = TRUE;
-	opd.driver_type = WDI_WINUSB;
-
 	while(1)
 	{
-		c = getopt_long(argc, argv, "n:f:m:d:c:v:p:i:l:t:o:hxsb", long_options, NULL);
+		c = getopt_long(argc, argv, "blhvp", long_options, NULL);
+		oprintf("got argument: %d \n", c);
 		if (c == -1)
 			break;
 		switch(c) {
-		case 1: // --stealth-cert
-			oic.disable_warning = TRUE;
-			break;
-		case 2: // --filter
-			oid.install_filter_driver = TRUE;
-			break;
-		case 'n':
-			dev.desc = optarg;
-			break;
-		case 'm':
-			opd.vendor_name = optarg;
-			break;
-		case 'f':
-			inf_name = optarg;
-			break;
-		case 'd':
-			ext_dir = optarg;
-			break;
-		case 'c':
-			cert_name = optarg;
-			break;
 		case 'v':
-			dev.vid = (unsigned short)strtol(optarg, NULL, 0);
-			break;
-		case 'o':
-			oid.pending_install_timeout = (DWORD)strtoul(optarg, NULL, 0);
-			break;
-		case 'p':
-			dev.pid = (unsigned short)strtol(optarg, NULL, 0);
-			break;
-		case 'i':
-			dev.is_composite = TRUE;
-			dev.mi = (unsigned char)strtol(optarg, NULL, 0);
-			break;
-		case 't':
-			opd.driver_type = (int)strtol(optarg, NULL, 0);
-			break;
-		case 'w':
-			opd.use_wcid_driver = TRUE;
+			opt_silent = 0;
 			break;
 		case 'h':
 			usage();
-			exit(0);
-			break;
-		case 'x':
-			opt_extract = 1;
-			break;
-		case 's':
-			opt_silent = 1;
-			log_level = WDI_LOG_LEVEL_NONE;
-			break;
-		case 'b':
-			oid.hWnd = (optarg)?(HWND)strtol(optarg, NULL, 0):GetConsoleHwnd();
-			oic.hWnd = oid.hWnd;
 			break;
 		case 'l':
-			log_level = (int)strtol(optarg, NULL, 0);
+			list_usbs=TRUE;
+			break;
+		case 'p':
+			pause=TRUE;
+			break;
+		case 'b':
+			bootloaderWinusbInstall = TRUE;
 			break;
 		default:
 			usage();
@@ -204,42 +179,25 @@ int __cdecl main(int argc, char** argv)
 
 	wdi_set_log_level(log_level);
 
-	oprintf("Extracting driver files...\n");
-	r = wdi_prepare_driver(&dev, ext_dir, inf_name, &opd);
-	oprintf("  %s\n", wdi_strerror(r));
-	if ((r != WDI_SUCCESS) || (opt_extract))
-		return r;
-
-	if (cert_name != NULL) {
-		oprintf("Installing certificate '%s' as a Trusted Publisher...\n", cert_name);
-		r = wdi_install_trusted_certificate(cert_name, &oic);
-		oprintf("  %s\n", wdi_strerror(r));
+	if (is_x64()) {
+		oprintf("I see you are on a 64 bit system, nice\n");
+	}
+	else {
+		oprintf("What a lovely 32 bit system you have\n");
 	}
 
-	oprintf("Installing driver(s)...\n");
-
-	// Try to match against a plugged device to avoid device manager prompts
-	matching_device_found = FALSE;
-	if (wdi_create_list(&ldev, &ocl) == WDI_SUCCESS) {
-		r = WDI_SUCCESS;
-		for (; (ldev != NULL) && (r == WDI_SUCCESS); ldev = ldev->next) {
-			if ( (ldev->vid == dev.vid) && (ldev->pid == dev.pid) && (ldev->mi == dev.mi) ) {
-				dev.hardware_id = ldev->hardware_id;
-				dev.device_id = ldev->device_id;
-				matching_device_found = TRUE;
-				oprintf("  %s: ", dev.hardware_id);
-				fflush(stdout);
-				r = wdi_install_driver(&dev, ext_dir, inf_name, &oid);
-				oprintf("%s\n", wdi_strerror(r));
-			}
-		}
+	if (list_usbs) {
+		*list=listDevices(opt_silent);
 	}
 
-	// No plugged USB device matches this one -> install driver
-	if (!matching_device_found) {
-		r = wdi_install_driver(&dev, ext_dir, inf_name, &oid);
-		oprintf("  %s\n", wdi_strerror(r));
+	if (bootloaderWinusbInstall) {
+		set_bootloader_to_winusb(opt_silent);
 	}
 
-	return r;
+	if (pause){
+		printf("Press Enter to continue\n");
+		getchar(); //hacky pause (press enter)
+	}
+	
+	return 0;
 }
---
+++ examples/wdi-simple.h
@@ -0,0 +1,3 @@
+#pragma once
+struct wdi_device_info listDevices(int);
+void set_bootloader_to_winusb(int);
